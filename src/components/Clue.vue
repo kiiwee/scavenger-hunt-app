e67c5df48e6f216b298a961778658911ba8a87fe
Stoev,Ilian I.S <i361859@fhict.nl>
2022-10-20 10:12:01 +0200

    first commit

---
+++ src/components/Clue.vue
@@ -0,0 +1,19 @@
+<template>
+    
+    <div class="w-full max-w-xs">
+ <div class="mb-6">  
+    
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <label class="block text-gray-700 text-s font-bold mb-2" >
+Winter is coming. What will you need to heat your shelter? The answer to this question, is also the name of the building you need to visit. Once you are there, you may need a printout...
+    </label></form>  </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,40 @@
+<template>
+    <div v-if="!check_show" class="w-full max-w-xs">
+  <form @submit.prevent class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    
+    <div class="mb-3">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input v-model="pass" class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-white-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+      <p class="text-red-500 text-xs italic">Please enter the password.</p>
+    </div>
+    <div class="flex items-center justify-between">
+      <button 
+ @click="checkpass" class="bg-blue-500 hover:bg-blue-700 text-white font-bold  py-2 px-20 rounded focus:outline-none focus:shadow-outline" type="button">
+        Reveal Clue
+      </button>
+    </div>
+  </form>
+  <p class="text-center text-gray-500 text-xs">
+    &copy;2022 Scavenger Hunt
+  </p>
+</div>
+<Clue v-if="check_show"></Clue>
+</template>
+
+<script setup lang="ts">
+import Clue from './Clue.vue'
+import { Console } from 'console';
+import { ref, onMounted } from 'vue'
+const pass=ref('')
+const check_show=ref(0)
+function checkpass() {
+  if (pass.value=="putka") {
+    console.log('Stana')
+    check_show.value=1
+  } else {
+    
+  }
+}
+</script>
